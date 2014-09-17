426b5e626e0b32338704ff97395b5230b0b05222
kyokomi <kyoko1220adword@gmail.com>
2014-09-18 01:21:57 +0900

    added CreateIssuesNote

--- gogitlab/notes.go
+++ gogitlab/notes.go
@@ -5,13 +5,13 @@ import (
 	"encoding/json"
 )
 
-
 const (
 	issuesNotesURL = "/projects/:id/issues/:issue_id/notes"
 	issuesNoteURL = "/projects/:id/issues/:issue_id/notes/:note_id"
 	createIssueNoteURL = "/projects/:id/issues/:issue_id/notes"
 )
 
+// Note are comments on snippets, issues or merge requests. struct
 type Note struct {
 	ID        int          `json:"id"`
 //	Attachment interface{} `json:"attachment"`
@@ -20,6 +20,7 @@ type Note struct {
 	CreatedAt string       `json:"created_at"`
 }
 
+// IssuesNotes GET a list of all notes for a single issue. by the authenticated user.
 func (g *Gitlab) IssuesNotes(projectID, issueID int) ([]*Note, error) {
 
 	param := map[string]string{
@@ -38,6 +39,7 @@ func (g *Gitlab) IssuesNotes(projectID, issueID int) ([]*Note, error) {
 	return notes, err
 }
 
+// IssuesNote GET a single note for a specific project issue. by the authenticated user.
 func (g *Gitlab) IssuesNote(projectID, issueID, noteID int) (*Note, error) {
 
 	param := map[string]string{
@@ -55,4 +57,16 @@ func (g *Gitlab) IssuesNote(projectID, issueID, noteID int) (*Note, error) {
 	}
 
 	return note, err
+}
+
+// CreateIssuesNote POST a new note to a single project issue. by the authenticated user.
+func (g *Gitlab) CreateIssuesNote(projectID, issueID int, data []byte) ([]byte, error) {
+
+	param := map[string]string{
+		":id":       strconv.Itoa(projectID),
+		":issue_id": strconv.Itoa(issueID),
+	}
+	url := g.ResourceUrl(createIssueNoteURL, param)
+
+	return g.buildAndExecRequest("POST", url, data)
 }
--- gogitlab/notes_test.go
+++ gogitlab/notes_test.go
@@ -3,6 +3,7 @@ package gogitlab
 import (
 	"github.com/stretchr/testify/assert"
 	"testing"
+	"net/url"
 )
 
 func TestIssuesNotes(t *testing.T) {
@@ -23,5 +24,17 @@ func TestIssuesNote(t *testing.T) {
 	assert.Equal(t, note.Body, "_Status changed to closed_")
 	assert.Equal(t, note.Author.ID, 1)
 	assert.Equal(t, note.CreatedAt, "2013-10-02T09:22:45Z")
+	defer ts.Close()
+}
+
+func TestCreateIssuesNote(t *testing.T) {
+	ts, gitlab := Stub("stubs/notes/create.json")
+
+	data := url.Values{}
+	data.Set("body", "hoge")
+
+	_, err := gitlab.CreateIssuesNote(1, 1, []byte(data.Encode()))
+
+	assert.Equal(t, err, nil)
 	defer ts.Close()
 }
